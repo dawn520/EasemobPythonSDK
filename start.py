25bedd7fc9f7fa47855f16f4b23ebccdc0c248b3
xixi <xixi@xixideMacBook-Pro.local>
2017-08-31 14:28:16 +0800

    add log

--- start.py
+++ start.py
@@ -8,6 +8,8 @@ import time
 
 import sys
 
+import logging
+
 from easemob.emclient import *
 from utils.DB import DB
 from utils.extract import extract
@@ -25,6 +27,21 @@ DEFAULT_REST = config.get('huanxin', 'default_rest')
 TOKEN = config.get('huanxin', 'token')
 LAST_GET_TIME = int(config.get('huanxin', 'last_get_time'))
 LAST_MESSAGE_TIME = int(config.get('huanxin', 'last_message_time'))
+# 开启日志
+# 第一步，创建一个logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logfile = os.path.join(BASE_DIR, "log/log.txt")
+fh = logging.FileHandler(logfile, mode='w')
+fh.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
 
 
 def do_it(message_time):
@@ -54,39 +71,39 @@ def do_it(message_time):
             db = DB(config)
             db.insert(newData)
     else:
-        print('请求错误:状态码' + str(response.code))
+        logger.info('请求错误:状态码' + str(response.code))
     return response.code
 
 
 if __name__ == '__main__':
     client = PyClient(APP_KEY, DEFAULT_REST, CLIENT_ID, CLIENT_SECRET)
     # 获取token
-    print('###任务开始执行')
+    logger.info('任务开始执行')
     nowTimestamp = int(time.time())
     if len(TOKEN) == 0 or nowTimestamp - LAST_GET_TIME > 86400:
-        print('####管理员token已过期，获取中……')
+        logger.info('管理员token已过期，获取中……')
         client.get_admin_token(CLIENT_ID, CLIENT_SECRET)
         config.set('huanxin', 'token', client.admin_token)
         config.set('huanxin', 'last_get_time', str(nowTimestamp))
         config.write(open(configFile, "w"))
     else:
         client.admin_token = TOKEN
-    print('####管理员token：' + client.admin_token)
+    logger.info('管理员token：' + client.admin_token)
     client.admin_rest_token = 'Bearer ' + client.admin_token
 
     if nowTimestamp - LAST_MESSAGE_TIME > 7200:
-        print('####开始执行未完成的任务')
+        logger.info('开始执行未完成的任务')
     while nowTimestamp - LAST_MESSAGE_TIME >= 7200:
         timeArray = time.localtime(LAST_MESSAGE_TIME)
         q = time.strftime("%Y%m%d%H", timeArray)
-        print('####获取' + q + "的聊天记录")
+        logger.info('获取' + q + "的聊天记录")
         result = do_it(q)
         if result == 0 or result == 404:
             LAST_MESSAGE_TIME += 3600
             config.set('huanxin', 'last_message_time', str(LAST_MESSAGE_TIME))
             config.write(open(configFile, "w"))
         if nowTimestamp - LAST_MESSAGE_TIME > 3600:
-            print('####休息1分钟')
+            logger.info('休息1分钟')
             time.sleep(50)
-    print('#####任务完成')
+    logger.info('任务完成')
     sys.exit()
